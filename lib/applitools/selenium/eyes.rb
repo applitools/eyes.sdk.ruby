ef6e13a37db348f8f0488e5223362254dfa5db88
nikita-andreev <nsa31415@gmail.com>
2018-02-28 14:16:47 +0200

    WIP: css border is included to the check_element result

--- lib/applitools/selenium/eyes.rb
+++ lib/applitools/selenium/eyes.rb
@@ -279,9 +279,14 @@ module Applitools::Selenium
                 target.coordinate_type
               end
 
-            region_provider = Applitools::RegionProvider.new(
+            stitch_region_provider = Applitools::RegionProvider.new(
               region_for_element(eyes_element), use_coordinates
             )
+
+            region_provider = Applitools::RegionProvider.new(
+                Applitools::Region.from_location_size(eyes_element.location, eyes_element.size), use_coordinates
+            )
+
           else
             # check_window
             logger.info "check_window(match_timeout: #{timeout}, tag: #{match_data.tag})"
@@ -301,7 +306,7 @@ module Applitools::Selenium
               eyes_element.overflow = 'hidden'
             end
 
-            region_provider = Applitools::RegionProvider.new(region_provider.region, target.coordinate_type)
+            region_provider = Applitools::RegionProvider.new((stitch_region_provider || region_provider).region, target.coordinate_type)
 
             self.region_to_check = region_provider
 
