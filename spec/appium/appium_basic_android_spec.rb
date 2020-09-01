b90ef1574dcdf3a89075ed1c2d3007ca3aa4658d
Nikita Andreyev <nsa31415@gmail.com>
2020-09-01 14:21:28 +0300

    Failed appium test is removed

--- spec/appium/appium_basic_android_spec.rb
+++ spec/appium/appium_basic_android_spec.rb
@@ -18,18 +18,18 @@ RSpec.describe 'Android Basic test', appium: true do
       expect(actual_ignore_regions).to include(Applitools::Region.new(151, 237, 90, 48))
     end
   end
-  it 'Appium_Android_CheckRegionWithIgnoreRegion', pending: true do
-    button.click
-    eyes.check(
-      'Image Container',
-      Applitools::Appium::Target.region(:id, 'com.applitools.helloworld.android:id/image_container').
-          ignore(label_in_container).
-          ignore(id: 'com.applitools.helloworld.android:id/image')
-    )
-    app_output(eyes.api_key).with_ignore_regions do |actual_ignore_regions|
-      fail
-    end
-  end
+  # it 'Appium_Android_CheckRegionWithIgnoreRegion', pending: true do
+  #   button.click
+  #   eyes.check(
+  #     'Image Container',
+  #     Applitools::Appium::Target.region(:id, 'com.applitools.helloworld.android:id/image_container').
+  #         ignore(label_in_container).
+  #         ignore(id: 'com.applitools.helloworld.android:id/image')
+  #   )
+  #   app_output(eyes.api_key).with_ignore_regions do |actual_ignore_regions|
+  #     fail
+  #   end
+  # end
   it 'Appium_Android_CheckRegion' do
     eyes.check('Button', Applitools::Appium::Target.region(button))
   end
