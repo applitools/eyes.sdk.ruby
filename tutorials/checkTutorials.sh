e69346554250c5ea1c23b62178fdf510405c8161
artem <artem.borodavka@applitools.com>
2020-09-08 15:26:36 +0300

    add comment

--- tutorials/checkTutorials.sh
+++ tutorials/checkTutorials.sh
@@ -2,6 +2,7 @@
 set -e
 docker run -e APPLITOOLS_API_KEY ruby_selenium_basic
 docker run -e APPLITOOLS_API_KEY ruby_selenium_ufg
+# There should be separate runs for the capybara and watir tutorials alongside with the separate tests runs
 #docker run -e APPLITOOLS_API_KEY ruby_capybara
 #docker run -e APPLITOOLS_API_KEY ruby_watir
 
