5fd3592d2a59e30795974eddb5dc84aff408de68
Yurii <yurii.cherepovskiy@applitools.com>
2021-05-06 18:32:14 +0300

    Set emitter and overrides to branch ruby_variantId

--- coverage_tests/configuration/index.js
+++ coverage_tests/configuration/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'eyes_selenium_ruby',
-  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/ruby/initialize.js',
-  overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/ruby/overrides.js',
+  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/ruby_variantId/ruby/initialize.js',
+  overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/ruby_variantId/ruby/overrides.js',
   template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/ruby/template.hbs',
   ext: '_spec.rb',
   outPath: './spec/coverage/generic'
