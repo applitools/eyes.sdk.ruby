7fd7e9d58aaa65de56f84ea214bde89a5c0ab999
Artem-applitools <59561599+Artem-applitools@users.noreply.github.com>
2021-12-08 13:33:40 +0200

    update drivers capabilities for safari 11 and firefox legacy (#295)
    update generic tests configs to point to the universal-sdk branch

--- coverage_tests/configuration/index.js
+++ coverage_tests/configuration/index.js
@@ -1,8 +1,11 @@
 module.exports = {
     name: 'eyes_selenium_ruby',
-    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/ruby/initialize.js',
-    overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/ruby/overrides.js',
-    template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/ruby/template.hbs',
+    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/ruby/initialize.js',
+    overrides: [
+        'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js'
+    ],
+    template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/ruby/template.hbs',
+    tests: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/coverage-tests.js',
     ext: '_spec.rb',
     outPath: './spec/coverage/generic'
 };
--- coverage_tests/spec/driver_capabilities.rb
+++ coverage_tests/spec/driver_capabilities.rb
@@ -2,11 +2,11 @@
 
 SAUCE_SERVER_URL = 'https://ondemand.saucelabs.com:443/wd/hub'
 SAUCE_CREDENTIALS = {
-  username: ENV['SAUCE_USERNAME'],
+    username: ENV['SAUCE_USERNAME'],
     accessKey: ENV['SAUCE_ACCESS_KEY']
 }.freeze
 BROWSER_OPTIONS_NAME = {
-  'chrome' => 'goog:chromeOptions',
+    'chrome' => 'goog:chromeOptions',
     'firefox' => 'moz:firefoxOptions'
 }.freeze
 FIREFOX_SERVER_URL = 'http://localhost:4445/wd/hub'
@@ -14,23 +14,23 @@ CHROME_SERVER_URL = 'http://localhost:4444/wd/hub'
 
 
 DEVICES = {
-  'Android Emulator' => {
-    type: 'sauce',
-      url: SAUCE_SERVER_URL,
-      capabilities: {
-        browserName: '',
-          deviceName: 'Android Emulator',
-          platformName: 'Android',
-          platformVersion: '6.0',
-          clearSystemFiles: true,
-          noReset: true
-      }.merge(SAUCE_CREDENTIALS)
-  },
-    'Pixel 3a XL' => {
-      type: 'sauce',
+    'Android Emulator' => {
+        type: 'sauce',
         url: SAUCE_SERVER_URL,
         capabilities: {
-          browserName: '',
+            browserName: '',
+            deviceName: 'Android Emulator',
+            platformName: 'Android',
+            platformVersion: '6.0',
+            clearSystemFiles: true,
+            noReset: true
+        }.merge(SAUCE_CREDENTIALS)
+    },
+    'Pixel 3a XL' => {
+        type: 'sauce',
+        url: SAUCE_SERVER_URL,
+        capabilities: {
+            browserName: '',
             deviceName: 'Google Pixel 3a XL GoogleAPI Emulator',
             platformName: 'Android',
             platformVersion: '10.0',
@@ -38,22 +38,22 @@ DEVICES = {
         }.merge(SAUCE_CREDENTIALS)
     },
     'Pixel 3 XL' => {
-      capabilities: {
-        browserName: '',
-          deviceName: 'Google Pixel 3 XL GoogleAPI Emulator',
-          platformName: 'Android',
-          platformVersion: '10.0',
-          deviceOrientation: 'portrait'
-      }.merge(SAUCE_CREDENTIALS),
+        capabilities: {
+            browserName: '',
+            deviceName: 'Google Pixel 3 XL GoogleAPI Emulator',
+            platformName: 'Android',
+            platformVersion: '10.0',
+            deviceOrientation: 'portrait'
+        }.merge(SAUCE_CREDENTIALS),
         url: SAUCE_SERVER_URL,
         sauce: true,
         type: 'sauce'
     },
     'Samsung Galaxy S8' => {
-      type: 'sauce',
+        type: 'sauce',
         url: SAUCE_SERVER_URL,
         capabilities: {
-          browserName: '',
+            browserName: '',
             name: 'Android Demo',
             platformName: 'Android',
             platformVersion: '7.0',
@@ -64,56 +64,56 @@ DEVICES = {
         }.merge(SAUCE_CREDENTIALS)
     },
     'iPhone 5S' => {
-      type: 'sauce',
+        type: 'sauce',
         url: SAUCE_SERVER_URL,
         capabilities: {
-          browserName: '',
+            browserName: '',
             deviceName: 'iPhone 5s Simulator',
             platformVersion: '12.4',
             platformName: 'iOS'
         }.merge(SAUCE_CREDENTIALS)
     },
     'iPhone 11 Pro' => {
-      type: 'sauce',
+        type: 'sauce',
         url: SAUCE_SERVER_URL,
         capabilities: {
-          browserName: '',
+            browserName: '',
             deviceName: 'iPhone 11 Pro Simulator',
             platformVersion: '13.4',
             platformName: 'iOS'
         }.merge(SAUCE_CREDENTIALS)
     },
     'iPhone XS' => {
-      type: 'sauce',
+        type: 'sauce',
         url: SAUCE_SERVER_URL,
         capabilities: {
-          browserName: '',
+            browserName: '',
             platformName: 'iOS',
             platformVersion: '13.0',
             deviceName: 'iPhone XS Simulator'
         }.merge(SAUCE_CREDENTIALS)
     },
     'iPad Air' => {
-      type: 'sauce',
+        type: 'sauce',
         url: SAUCE_SERVER_URL,
         capabilities: {
-          browserName: '',
+            browserName: '',
             deviceName: 'iPad Air Simulator',
             platformVersion: '12.4',
             platformName: 'iOS'
         }.merge(SAUCE_CREDENTIALS)
     },
     'Android 8.0 Chrome Emulator' => {
-      type: 'chrome',
+        type: 'chrome',
         url: CHROME_SERVER_URL,
         capabilities: {
-          browserName: 'chrome',
+            browserName: 'chrome',
             BROWSER_OPTIONS_NAME['chrome'] => {
-              mobileEmulation: {
-                deviceMetrics: {width: 384, height: 512, pixelRatio: 2},
-                  userAgent:
-                      'Mozilla/5.0 (Linux; Android 8.0.0; Android SDK built for x86_64 Build/OSR1.180418.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36'
-              },
+                mobileEmulation: {
+                    deviceMetrics: {width: 384, height: 512, pixelRatio: 2},
+                    userAgent:
+                        'Mozilla/5.0 (Linux; Android 8.0.0; Android SDK built for x86_64 Build/OSR1.180418.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36'
+                },
                 args: ['hide-scrollbars']
             }
         }
@@ -121,96 +121,110 @@ DEVICES = {
 }.freeze
 
 BROWSERS = {
-  'edge-18' => {
-    type: 'sauce',
-      url: SAUCE_SERVER_URL,
-      capabilities: {
-        browserName: 'MicrosoftEdge',
-          browserVersion: '18.17763',
-          platformName: 'Windows 10'
-      },
-      options: {
-        name: 'Edge 18',
-          avoidProxy: true,
-          screenResolution: '1920x1080'
-      }.merge(SAUCE_CREDENTIALS)
-  },
-    'ie-11' => {
-      type: 'sauce',
+    'edge-18' => {
+        type: 'sauce',
         url: SAUCE_SERVER_URL,
         capabilities: {
-          w3c: {
-            browserName: 'internet explorer',
-              browserVersion: '11.285',
-              platformName: 'Windows 10'
-          },
+            browserName: 'MicrosoftEdge',
+            browserVersion: '18.17763',
+            platformName: 'Windows 10'
+        },
+        options: {
+            name: 'Edge 18',
+            avoidProxy: true,
+            screenResolution: '1920x1080'
+        }.merge(SAUCE_CREDENTIALS)
+    },
+    'ie-11' => {
+        type: 'sauce',
+        url: SAUCE_SERVER_URL,
+        capabilities: {
+            w3c: {
+                browserName: 'internet explorer',
+                browserVersion: '11.285',
+                platformName: 'Windows 10'
+            },
             legacy: {
-              browserName: 'internet explorer',
+                browserName: 'internet explorer',
                 platform: 'Windows 10',
                 version: '11.285'
             }
         },
         options: {
-          name: 'IE 11',
+            name: 'IE 11',
             screenResolution: '1920x1080'
         }.merge(SAUCE_CREDENTIALS)
     },
     'safari-11' => {
-      type: 'sauce',
+        type: 'sauce',
         url: SAUCE_SERVER_URL,
         capabilities: {
-          w3c: {
-            browserName: 'safari',
-              browserVersion: '11.0',
-              platformName: 'macOS 10.12'
-          },
+            w3c: {
+                browserName: 'safari',
+                browserVersion: '11.1',
+                platformName: 'macOS 10.13'
+            },
             legacy: {
-              browserName: 'safari',
-                version: '11.0',
-                platform: 'macOS 10.12'
+                browserName: 'safari',
+                version: '11.1',
+                platform: 'macOS 10.13'
             }
         },
         options: {
-          name: 'Safari 11',
+            name: 'Safari 11',
             seleniumVersion: '3.4.0'
         }.merge(SAUCE_CREDENTIALS)
     },
     'safari-12' => {
-      type: 'sauce',
+        type: 'sauce',
         url: SAUCE_SERVER_URL,
         capabilities: {
-          w3c: {
-            browserName: 'safari',
-              browserVersion: '12.1',
-              platformName: 'macOS 10.13'
-          },
+            w3c: {
+                browserName: 'safari',
+                browserVersion: '12.1',
+                platformName: 'macOS 10.13'
+            },
             legacy: {
-              browserName: 'safari',
+                browserName: 'safari',
                 version: '12.1',
                 platform: 'macOS 10.13'
             }
         },
         options: {
-          name: 'Safari 12',
+            name: 'Safari 12',
             seleniumVersion: '3.4.0'
         }.merge(SAUCE_CREDENTIALS)
     },
+    'firefox-48' => {
+        type: 'sauce',
+        url: SAUCE_SERVER_URL,
+        capabilities: {
+            legacy: {
+                browserName: 'firefox',
+                platform: 'Windows 10',
+                version: '48.0'
+            }
+        },
+        options: {
+            name: 'Firefox 48'
+        }.merge(SAUCE_CREDENTIALS)
+    },
     'firefox' => {
-      type: 'firefox',
+        type: 'firefox',
         url: FIREFOX_SERVER_URL,
         capabilities: {
-          browserName: 'firefox',
+            browserName: 'firefox',
             BROWSER_OPTIONS_NAME['firefox'] => {
-              args: []
+                args: []
             }
         }
     },
     'chrome' => {
-      type: 'chrome',
+        type: 'chrome',
         capabilities: {
-          browserName: 'chrome',
+            browserName: 'chrome',
             BROWSER_OPTIONS_NAME['chrome'] => {
-              args: []
+                args: []
             }
         }
     }
