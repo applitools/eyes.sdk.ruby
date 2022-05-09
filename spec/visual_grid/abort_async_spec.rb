1e61cbe9a9360cc4706204ada200d6b3711b731a
Serhii Ovchynnyk <sergii.ovchynnyk@applitools.com>
2022-05-10 00:27:54 +0300

    just skip that hooks (abort_async_spec)

--- spec/visual_grid/abort_async_spec.rb
+++ spec/visual_grid/abort_async_spec.rb
@@ -5,9 +5,9 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 Applitools::EyesLogger.log_handler = Logger.new(STDOUT) unless ENV['TRAVIS']
 
 RSpec.describe 'Abort Async' do
-  before(:context) do |example|
-    @runner = Applitools::Selenium::VisualGridRunner.new(5) unless example.metadata[:skip]
-  end
+  # before(:context) do
+  #   @runner = Applitools::Selenium::VisualGridRunner.new(5)
+  # end
 
   let(:eyes) { Applitools::Selenium::Eyes.new(runner: @runner) }
   let(:web_driver) { Selenium::WebDriver.for :chrome }
@@ -20,13 +20,13 @@ RSpec.describe 'Abort Async' do
     )
   end
 
-  after do |example|
-    eyes.abort_async unless example.metadata[:skip]
-  end
-
-  after(:context) do |example|
-    @runner.get_all_test_results(false) unless example.metadata[:skip]
-  end
+  # after do
+  #   eyes.abort_async
+  # end
+  #
+  # after(:context) do
+  #   @runner.get_all_test_results(false)
+  # end
 
   it 'simple test', skip: true do
     driver.get('https://applitools.com')
