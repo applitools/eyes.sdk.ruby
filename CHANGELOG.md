47307c7e5992412847ecc88ed5aa60b604deeddf
Serhii Ovchynnyk <sergii.ovchynnyk@applitools.com>
2022-05-18 06:26:40 +0300

    Bump version: 4.0.4 → 4.0.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## [vNext]
+## [4.0.5] - 2022-05-18
 ### Updated
 - Update version of Universal SDK to 2.5.11
 ### Fixed
--- lib/applitools/version.rb
+++ lib/applitools/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: false
 
 module Applitools
-  VERSION = '4.0.5.a'.freeze
+  VERSION = '4.0.5'.freeze
   UNIVERSAL_VERSION = '2.5.11'.freeze
 end
