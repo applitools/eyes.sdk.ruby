d58acad0dc7b7288a4c0c287edd2779f9243fabb
Serhii Ovchynnyk <sergii.ovchynnyk@applitools.com>
2022-06-13 17:21:46 +0300

    add retry on socket hang up

--- lib/applitools/universal_sdk/universal_eyes_manager.rb
+++ lib/applitools/universal_sdk/universal_eyes_manager.rb
@@ -19,9 +19,13 @@ module Applitools
 
       @eyes = @universal_client.eyes_manager_make_eyes(@manager, driver_config_json, config)
 
+      if @eyes[:message] === 'socket hang up'
+        Applitools::EyesLogger.logger.error "#{Applitools::Connectivity::UniversalClient::EYES_MANAGER_MAKE_EYES} : socket hang up ; retry"
+        @eyes = @universal_client.eyes_manager_make_eyes(@manager, driver_config_json, config)
+      end
       if @eyes[:message] && @eyes[:stack]
         Applitools::EyesLogger.logger.error "Eyes not opened: #{@eyes[:message]}"
-        Applitools::EyesLogger.logger.error "Stack for #{Applitools::Connectivity::UniversalClient::EYES_MANAGER_MAKE_EYES} : #{@eyes[:stack]}"
+        Applitools::EyesLogger.logger.debug "Stack for #{Applitools::Connectivity::UniversalClient::EYES_MANAGER_MAKE_EYES} : #{@eyes[:stack]}"
         return nil
       end
 
