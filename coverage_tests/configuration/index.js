b2fdb734ab2c5bb7be989c10b370072d8a1b0619
artem <artem.borodavka@applitools.com>
2022-07-07 16:16:56 +0300

    run only horizontal scroll tests on travis

--- coverage_tests/configuration/index.js
+++ coverage_tests/configuration/index.js
@@ -8,5 +8,6 @@ module.exports = {
     template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/ruby/template.hbs',
     tests: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/coverage-tests.js',
     ext: '_spec.rb',
-    outPath: './spec/coverage/generic'
+    outPath: './spec/coverage/generic',
+    emitOnly: ['/check window fully on page with horizontal scroll/']
 };
